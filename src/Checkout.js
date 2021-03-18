1ae806a801a044a60ab9cb1ed66740d68c84a73b
Dzhani Ivanov <djani.ivanov@abv.bg>
2021-03-18 15:16:37 +0200

    update checkout,add productcart

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import './Checkout.css'
 import Subtotal from './Subtotal';
+import { useStateValue } from './StateProvider';
+import ProductCart from './ProductCart';
+import { IsoOutlined } from '@material-ui/icons';
 
 function Checkout() {
+    const [{ basket }] = useStateValue();
     return (
         <div className="checkout">
             <div className="checkout_left">
-                <h2 className="checkout_title">Your shopping basket is empty.</h2>
-                <p>You have no items in your basket. Buy one.</p>
+
+                {
+                    basket.length === 0 ? (
+                        <div>
+                            <h2 className="checkout_title">Your shopping basket is empty.</h2>
+                            <p>You have no items in your basket. Buy one.</p>
+                        </div>
+                    ) : (
+                        <div>
+                            <h2 className="shoppingbaskettitle">Items in the Shopping Basket</h2>
+                            {
+                                basket.map(item=>(
+                                    <ProductCart
+                                        id={item.id}
+                                        title={item.title}
+                                        image={item.image}
+                                        price={item.price}
+                                        rating={item.rating}
+                                    />
+                                ))
+                            }
+                        </div>
+                    )
+                }
+
             </div>
             <div className="checkout_right">
                 <Subtotal />
