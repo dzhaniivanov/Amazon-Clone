0bb269331a25defe3123192fe1998d6456d0a313
Dzhani Ivanov <djani.ivanov@abv.bg>
2021-03-29 22:19:20 +0300

    .

--- src/Home.js
+++ src/Home.js
@@ -5,11 +5,12 @@ import './Home.css';
 function Home() {
     return (
         <div className="home">
-            <img
+         {/*    <img
                 className="home__image"
                 src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
                 alt=""
-            />
+            /> */}
+            <img src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Computers_1x._CB432469755_.jpg" alt="home wallpaper" className="home__image"/>
             <div className="home__row">
                 <Product
                     id="1"
