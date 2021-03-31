ee76701eb6fc25b80643c5d1cd314bcafc833381
Dzhani Ivanov <djani.ivanov@abv.bg>
2021-03-31 21:01:39 +0300

    updated about page

--- src/About.js
+++ src/About.js
@@ -1,9 +1,36 @@
+import './About.css'
+
+
 
 function About() {
     return (
-        <div>
-            <h1>About page</h1>
-            
+        <div className="about">
+            <div className="newsfeed">
+                <div className="title">
+                    Newsfeed
+                </div>
+                <span className="newsfeedItems">
+                    Amazon India ranks among top 3 on the list of India's coolest workplace
+                </span>
+                <span className="newsfeedItems">
+                    Amazon India ranks among top 3 on the list of India's coolest workplace
+                </span>
+                <span className="newsfeedItems">
+                    Amazon India ranks among top 3 on the list of India's coolest workplace
+                </span>
+                <span className="newsfeedItems">
+                    Amazon India ranks among top 3 on the list of India's coolest workplace
+                </span>
+                <span className="newsfeedItems">
+                    Amazon India ranks among top 3 on the list of India's coolest workplace
+                </span>
+            </div>
+            <div className="centerInfo">
+
+            </div>
+            <div className="topStories">
+
+            </div>
         </div>
     )
 }
