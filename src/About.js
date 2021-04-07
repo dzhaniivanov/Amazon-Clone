f1d867b0d673bf0a4c34e136329e9b94c1fd6631
Dzhani Ivanov <djani.ivanov@abv.bg>
2021-04-07 11:28:24 +0300

    simple about page with info added

--- src/About.js
+++ src/About.js
@@ -5,48 +5,11 @@ import './About.css'
 function About() {
     return (
         <div className="about">
-            <div className="newsfeed">
-                <div className="title">
-                    Newsfeed
-                </div>
-                <span className="newsfeedItems">
-                    Amazon India ranks among top 3 on the list of India's coolest workplace
-                </span>
-                <span className="newsfeedItems">
-                    Amazon India ranks among top 3 on the list of India's coolest workplace
-                </span>
-                <span className="newsfeedItems">
-                    Amazon India ranks among top 3 on the list of India's coolest workplace
-                </span>
-                <span className="newsfeedItems">
-                    Amazon India ranks among top 3 on the list of India's coolest workplace
-                </span>
-                <span className="newsfeedItems">
-                    Amazon India ranks among top 3 on the list of India's coolest workplace
-                </span>
-            </div>
-            <div className="centerInfo">
-            <img src="https://assets.aboutamazon.com/dims4/default/9f11fc5/2147483647/strip/true/crop/1920x1081+0+238/resize/1320x743!/format/webp/quality/90/?url=https%3A%2F%2Famazon-blogs-brightspot.s3.amazonaws.com%2Ffb%2F5c%2F6f4b53e249d1a24b94db3fad7cdb%2Faerial-reduced.jpg" alt="img" className="image"/>
-         
-
-
-            </div>
-            <div className="topStories">
-                <div className="title">Top Stories</div>
-                <div className="topStoriesItem">
-                    Cloud computing pioners's new focus in...
-                </div>
-                <div className="topStoriesItem">
-                    Cloud computing pioners's new focus in...
-                </div>
-                <div className="topStoriesItem">
-                    Cloud computing pioners's new focus in...
-                </div>
-                <div className="topStoriesItem">
-                    Cloud computing pioners's new focus in...
-                </div>
-
-            </div>
+                <h1>About Amazon Clone</h1>
+                <h2>Author:Dzhani Chavdarov Ivanov</h2>
+                <h3>djani.ivanov94@gmail.com</h3>
+                <h4>https://github.com/dzhaniivanov94</h4>
+                <h4>https://www.linkedin.com/in/dzhani-ivanov-182412206/</h4>
         </div>
     )
 }
